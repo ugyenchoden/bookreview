7c882e3e2e9064e3b2cfb0cd7087ce0b009d290c
ugyenchoden <ugyen.choden@selise.ch>
2023-11-08 17:47:41 +0600

    Initial

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+function Nav(){
+    return (
+        <nav className="navigation">
+            <ul>
+                <li>Home</li>
+                <li>Articles</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Nav;
